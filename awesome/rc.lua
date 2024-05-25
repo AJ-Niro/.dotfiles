fed5705a9f026fe96870a1d3947d0941f920b5bd
AndresJoaquinNino <andresninorodil@gmail.com>
2024-05-25 18:39:35 -0400

    awesome: make tag and wibar bigger

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -193,7 +193,22 @@ awful.screen.connect_for_each_screen(function(s)
     set_wallpaper(s)
 
     -- Each screen has its own tag table.
-    awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
+    local custom_tag_list = {
+        "   1   ",
+        "   2   ",
+        "   3   ",
+        "   4   ",
+        "   5   ",
+        "   6   ",
+        "   7   ",
+        "   8   ",
+        "   9   ",
+    }
+    awful.tag(
+        custom_tag_list,
+        s,
+        awful.layout.layouts[0]
+    )
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
@@ -220,7 +235,7 @@ awful.screen.connect_for_each_screen(function(s)
     }
 
     -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s })
+    s.mywibox = awful.wibar({ position = "top", screen = s, height = 30 })
 
     -- Add widgets to the wibox
     s.mywibox:setup {
