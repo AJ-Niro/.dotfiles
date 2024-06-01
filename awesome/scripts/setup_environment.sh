4730bc39f7707bf3df11a0283c989d84afefd57d
AndresJoaquinNino <andresninorodil@gmail.com>
2024-06-01 19:53:45 -0400

    feat: enable natural scrolling for Touchpad

--- awesome/scripts/setup_environment.sh
+++ awesome/scripts/setup_environment.sh
@@ -1,2 +1,5 @@
 # Change laptop Resolution
 xrandr --output eDP --mode 1280x720 --rate 60.02
+
+# Set natural scroll for the Touchpad
+xinput --set-prop "SYNA32AC:00 06CB:CD50 Touchpad" "libinput Natural Scrolling Enabled" 1
