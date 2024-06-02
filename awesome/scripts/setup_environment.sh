ad84aa2d8cdf1466fcf47c166638fc2073525505
AndresJoaquinNino <andresninorodil@gmail.com>
2024-06-01 20:02:11 -0400

    feat: configure tapping for touchpad

--- awesome/scripts/setup_environment.sh
+++ awesome/scripts/setup_environment.sh
@@ -1,5 +1,7 @@
 # Change laptop Resolution
 xrandr --output eDP --mode 1280x720 --rate 60.02
 
-# Set natural scroll for the Touchpad
-xinput --set-prop "SYNA32AC:00 06CB:CD50 Touchpad" "libinput Natural Scrolling Enabled" 1
+# Configure touchpad tap and sroll
+TOUCHPAD_NAME="SYNA32AC:00 06CB:CD50 Touchpad"
+xinput --set-prop "$TOUCHPAD_NAME" "libinput Natural Scrolling Enabled" 1
+xinput --set-prop "$TOUCHPAD_NAME" "libinput Tapping Enabled" 1
