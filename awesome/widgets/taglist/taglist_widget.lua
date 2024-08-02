090e16b4cdf76fcccc29d787b37a3ec3b446ac12
AndresJoaquinNino <andresninorodil@gmail.com>
2024-08-01 22:09:58 -0400

    feat(awesome): make tags a ring with inside circle

--- awesome/widgets/taglist/taglist_widget.lua
+++ awesome/widgets/taglist/taglist_widget.lua
@@ -1,9 +1,9 @@
 local gears = require("gears")
 local awful = require("awful")
 local wibox = require("wibox")
+local beautiful = require("config.custom_beautiful")
 
 local taglist_widget = function(s)
-
   local taglist_buttons = gears.table.join(
     awful.button({}, 1, function(t) t:view_only() end),
     awful.button({ modkey }, 1, function(t)
@@ -22,11 +22,55 @@ local taglist_widget = function(s)
   )
 
   local local_taglist = awful.widget.taglist {
-    screen = s,
-    filter = awful.widget.taglist.filter.all,
-    buttons = taglist_buttons,
-    style = {
-      shape = gears.shape.rounded_bar,
+    screen          = s,
+    filter          = awful.widget.taglist.filter.all,
+    buttons         = taglist_buttons,
+    style           = {
+      shape = gears.shape.rounded_rect,
+    },
+    layout          = {
+      spacing = 5,
+      layout  = wibox.layout.fixed.horizontal
+    },
+    widget_template = {
+      {
+        {
+          id     = 'index_role',
+          widget = wibox.widget.textbox,
+          markup = '',
+        },
+        id                 = "index_bg",
+        widget             = wibox.container.background,
+        bg                 = beautiful.bg_normal,
+        shape              = gears.shape.circle,
+        shape_border_width = 6,
+        shape_border_color = beautiful.bg_normal,
+      },
+      widget             = wibox.container.background,
+      bg                 = beautiful.bg_normal,
+      shape              = gears.shape.circle,
+      shape_border_width = 3,
+      shape_border_color = beautiful.fg_normal,
+      shape_clip         = true,
+      forced_height      = beautiful.font_size * 2,
+      forced_width       = beautiful.font_size * 2,
+
+      create_callback    = function(self, current_tag)
+        local index_bg = self:get_children_by_id('index_bg')
+        if current_tag.selected then
+          index_bg[1].bg = beautiful.fg_normal
+        else
+          index_bg[1].bg = beautiful.bg_normal
+        end
+      end,
+      update_callback    = function(self, current_tag)
+        local index_bg = self:get_children_by_id('index_bg')
+        if current_tag.selected then
+          index_bg[1].bg = beautiful.fg_normal
+        else
+          index_bg[1].bg = beautiful.bg_normal
+        end
+      end,
     },
   }
 
