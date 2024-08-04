c275f41d01978f60ac1bebf090f748720899f53f
AndresJoaquinNino <andresninorodil@gmail.com>
2024-08-04 19:52:22 -0400

    refactor(awesome): move tasklist button to widget file

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -174,28 +174,6 @@ mykeyboardlayout = awful.widget.keyboardlayout()
 -- Create a textclock widget
 mytextclock = wibox.widget.textclock()
 
-local tasklist_buttons = gears.table.join(
-    awful.button({}, 1, function(c)
-        if c == client.focus then
-            c.minimized = true
-        else
-            c:emit_signal(
-                "request::activate",
-                "tasklist",
-                { raise = true }
-            )
-        end
-    end),
-    awful.button({}, 3, function()
-        awful.menu.client_list({ theme = { width = 250 } })
-    end),
-    awful.button({}, 4, function()
-        awful.client.focus.byidx(1)
-    end),
-    awful.button({}, 5, function()
-        awful.client.focus.byidx(-1)
-    end))
-
 local function set_wallpaper(s)
     -- Wallpaper
     if beautiful.wallpaper then
--- awesome/widgets/tasklist/tasklist_widget.lua
+++ awesome/widgets/tasklist/tasklist_widget.lua
@@ -1,6 +1,29 @@
 local awful = require("awful")
 local wibox = require("wibox")
+local gears = require("gears")
 local beautiful = require("config.custom_beautiful")
+
+local tasklist_buttons = gears.table.join(
+    awful.button({}, 1, function(c)
+        if c == client.focus then
+            c.minimized = true
+        else
+            c:emit_signal(
+                "request::activate",
+                "tasklist",
+                { raise = true }
+            )
+        end
+    end),
+    awful.button({}, 3, function()
+        awful.menu.client_list({ theme = { width = 250 } })
+    end),
+    awful.button({}, 4, function()
+        awful.client.focus.byidx(1)
+    end),
+    awful.button({}, 5, function()
+        awful.client.focus.byidx(-1)
+    end))
 
 local tasklist_widget = function(s)
   return awful.widget.tasklist {
@@ -35,7 +58,7 @@ local tasklist_widget = function(s)
           id     = 'clienticon',
           widget = awful.widget.clienticon,
         },
-        margins = 2,
+        margins = 4,
         widget  = wibox.container.margin
       },
       nil,
