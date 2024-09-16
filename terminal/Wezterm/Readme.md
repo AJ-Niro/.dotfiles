f95a9c1b171b16873bb387386cbc42002135f900
AndresJoaquinNino <andresninorodil@gmail.com>
2024-09-16 12:46:27 -0400

    fea(wezterm): add wezterm config

---
+++ terminal/Wezterm/Readme.md
@@ -0,0 +1,8 @@
+## Pre-install
+
+* [Follow the official doc for installation](https://wezfurlong.org/wezterm/install/linux.html#using-the-apt-repo)
+
+
+## Install
+
+* `ln -s ~/.dotfiles/terminal/Wezterm/wezterm.lua  $HOME/.config/wezterm/wezterm.lua`
---
+++ terminal/Wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font 'Hack Nerd Font'
+
+config.colors = {
+    -- The default text color
+    foreground = 'rgb(223, 223, 223)',
+
+    -- The default background color
+    background = 'rgb(26, 26, 26, 75%)',
+
+    -- Make the selection text color fully transparent.
+    selection_fg = 'none',
+
+    -- Set the selection background color with alpha.
+    selection_bg = 'rgba(50% 50% 50% 50%)',
+}
+  
+
+return config
